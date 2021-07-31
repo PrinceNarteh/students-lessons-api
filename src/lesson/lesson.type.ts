4b70ad380944d74288918c55c607acd57435eaa0
Prince Narteh <prinarteh@gmail.com>
2021-07-31 16:03:28 +0000

    Add Lesson entity

--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -4,10 +4,13 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 export class LessonType {
   @Field((type) => ID)
   id: string;
+
   @Field()
   name: string;
+
   @Field()
   startDate: string;
+
   @Field()
   endDate: string;
 }
