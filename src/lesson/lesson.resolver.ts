df152eb2c423f267be79c7d6ade52cb78c8f0fe6
Prince Narteh <prinarteh@gmail.com>
2021-08-01 20:37:36 +0000

    Add getLesson, getAllLessons and createLesson resolver

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,27 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private readonly lessonService: LessonService) {}
+
+  @Query((returns) => [LessonType])
+  getAllLessons() {
+    return this.lessonService.getAllLessons();
+  }
+
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: '123456',
-      name: 'English Language',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  getLesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
